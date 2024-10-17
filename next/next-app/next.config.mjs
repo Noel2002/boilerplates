17e4786d5747e6715a901c3621fab40ded25a052
Noel2002 <berwanoel@gmail.com>
2024-10-17 18:31:16 +0800

    configured fonts, shadcn and tested rewrite routes

--- next/next-app/next.config.mjs
+++ next/next-app/next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  images: {
+    domains: [
+      'res.cloudinary.com'
+    ]
+  },
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: 'https://jsonplaceholder.typicode.com/:path*',
+    },
+  ],
 };
 
 export default nextConfig;
